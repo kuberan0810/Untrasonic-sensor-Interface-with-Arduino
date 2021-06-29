73701ce2379fd4c7235307d8eaa4adffaa62cc4d
Kubera  Murthi <70520314+kuberan0810@users.noreply.github.com>
2021-06-29 19:46:27 +0530

    Add files via upload

---
+++ Ultrasonic.ino
@@ -0,0 +1,28 @@
+void setup() {
+
+pinMode (10,INPUT);
+pinMode (9,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() 
+{
+  //Send trigger pulse
+  digitalWrite(9,LOW);
+  delayMicroseconds(2);
+  digitalWrite(9,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(9,LOW);
+
+  //Recieve pulse
+  long t = pulseIn(10,HIGH);
+
+  // Distance = spped*time
+  // 343m/s is speed of sound
+  int d = t*0.343/2;
+  Serial.print("Distance = ");
+  Serial.println(d);
+
+  
+}
+
